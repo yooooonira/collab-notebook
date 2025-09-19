a6c8422f2c2dcb7b3db87b93dc6a466bf885da73
yoooonira <yooooonira@gmail.com>
2025-09-19 11:18:11 +0900

    feat(user): role 커스터마이징을 위한 URL 추가 및 MyInfo는 조회 전용으로 수정

--- drf_api/user/urls.py
+++ drf_api/user/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # signup, login은 Supabase에서 처리하니까 drf에서는 필요없다. 
+    path("auth/signup/", views.SignupView.as_view(), name="signup"),
     path("auth/me/", views.MyInfo.as_view(), name="my-info"),
     path("users/search/", views.Search.as_view(), name="search"),
 ]
+  
