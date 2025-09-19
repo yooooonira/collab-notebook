2b2fbade8814c28ed7163191aa1f565f8f06775c
yoooonira <yooooonira@gmail.com>
2025-09-19 15:00:14 +0900

    feat(user): add available user list with tests Closes #5

--- drf_api/user/urls.py
+++ drf_api/user/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("auth/signup/", views.SignupView.as_view(), name="signup"),
     path("auth/me/", views.MyInfo.as_view(), name="my-info"),
     path("users/search/", views.Search.as_view(), name="search"),
+    path("users/available/", views.AvailableUserList.as_view(), name="user-available"), #권한부여용 이용자 목록 조회 
 ]
   
